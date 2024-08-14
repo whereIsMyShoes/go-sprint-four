3b36ad4b2e8d52913e3da7578cc68c54da74cc1e
Ruslan Mishkhozhev <depalme@yandex.ru>
2024-08-14 10:54:53 +0300

    second-commit

--- ftracker.go
+++ ftracker.go
@@ -101,7 +101,7 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	// ваш код здесь
-	return ((walkingCaloriesWeightMultiplier*weight + (math.Pow((meanSpeed(action, duration))*kmhInMsec, 2)/height)*0.029*weight) * duration * minInH)
+	return ((walkingCaloriesWeightMultiplier*weight + (math.Pow((meanSpeed(action, duration)*kmhInMsec), 2)/(height/cmInM))*0.029*weight) * duration * minInH)
 }
 
 // Константы для расчета калорий, расходуемых при плавании.
